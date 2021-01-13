2d96cf028b968f730f5b9a52ca719feaf68265aa
Ujjwal Rastogi <rastogiujj12@gmail.com>
2021-01-13 19:54:46 +0530

    scourInternet

--- service-admin/public/service-worker.js
+++ service-admin/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8906d6562b71a3a2969038a17987028e.js"
+  "/precache-manifest.f2ed8ad41c86680d9c4e6acf84696485.js"
 );
 
 workbox.clientsClaim();
